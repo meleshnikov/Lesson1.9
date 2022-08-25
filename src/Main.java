327fee9a76b6b6fbe1616a60cb245bed4743dbf6
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-26 00:23:16 +0300

    task1 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Лев", "Толстой");
+        Book book1 = new Book("Война и мир", author1, 1867);
+        Author author2 = new Author("Александр", "Пушкин");
+        Book book2 = new Book("Капитанская дочка", author2, 1836);
+        System.out.println("book2.getPublicationYear() = " + book2.getPublicationYear());
+        book2.setPublicationYear(1845);
+        System.out.println("book2.getPublicationYear() = " + book2.getPublicationYear());
     }
+
 }
